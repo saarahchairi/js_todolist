e5b4ff179f8f689147beb3e186656ffd1766e206
saarahchairi <sarni555@hotmail.com>
2022-06-02 16:52:05 +0200

    paramettre des boutons mis en place

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 let nomNouvelleTache = document.querySelector("input");
 let boutonAjouter = document.querySelectorAll("button")[0];
-let tacheSection = document.querySelectorAll("div")[0];
+let boutonTermine = document.querySelectorAll("button")[2];
+let boutonTout = document.querySelectorAll("button")[3];
+let tacheSection = document.querySelectorAll("div")[3];
 
+//Création de nouvelle tâche lorsque l'utilisateur clique sur le bouton "Ajouter"
 boutonAjouter.addEventListener("click", function () {
     let nouvelleTache = document.createElement("div");
     let titreTache = document.createElement("h4");
@@ -21,5 +24,50 @@ boutonAjouter.addEventListener("click", function () {
     nouvelleTache.append(titreTache, logo)
     titreTache.innerText = nomNouvelleTache.value
     tacheSection.appendChild(nouvelleTache);
-    nouvelleTache.setAttribute("class", "nouvelleTache")
+    nouvelleTache.setAttribute("class", "nouvelleTache");
+    //Déclarer une tâche comme étant terminée
+    let terminer = [];
+    fini.addEventListener("click", function () {
+        // terminer.push(nouvelleTache);
+        nouvelleTache.classList.add("done");
+        fini.style.color = "#60C5FF";
+        console.log(tacheSection);
+    });
+    
+    //Permettre de filter les tâches en affichant juste les éléments terminé
+    boutonTermine.addEventListener("click", function () {
+        tacheSection.removeChild(nouvelleTache)
+        // forEach(element => {
+        //     if (element.classList != "done"){
+        //         nouvelleTache.style.display = "none"
+        //     }
+        // });
+    })
+
+    //Permet d'afficher toutes les tâches
+    boutonTout.addEventListener("click", function () {
+        tacheSection.appendChild(nouvelleTache);
+    })
+
+    //Permettre à l'utilisateur de pouvoir rennomer la tâche
+    rennomer.addEventListener("click", function () {
+        rennomer.classList.replace("fa-pen-to-square", "fa-floppy-disk");
+        fini.style.visibility = "hidden";
+        poubelle.style.visibility = "hidden";
+        titreTache.replaceWith(document.createElement("input"));
+        
+        rennomer.addEventListener("click", function () {
+            rennomer.classList.replace("fa-floppy-disk", "fa-pen-to-square");
+            fini.style.visibility = "visible";
+            poubelle.style.visibility = "visible";
+            // let newTitre = titreTache.value;
+            titreTache.replaceWith(document.createElement("h4"));
+            // titreTache.innerText = newTitre;
+            // console.log(newTitre);
+        })
+    });
+    //Permet de supprimer la tâche
+    poubelle.addEventListener("click", function () {
+        nouvelleTache.remove(titreTache);
+    })
 });
