f316f819a96804d0ea4fbc97ff9c216307df3e69
saarahchairi <sarni555@hotmail.com>
2022-06-03 13:51:04 +0200

    boutons fonctionnelles

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,9 @@ let nomNouvelleTache = document.querySelector("input");
 let boutonAjouter = document.querySelectorAll("button")[0];
 let boutonTermine = document.querySelectorAll("button")[2];
 let boutonTout = document.querySelectorAll("button")[3];
+let boutonAFaire = document.querySelectorAll("button")[1];
 let tacheSection = document.querySelectorAll("div")[3];
+let tache = []; 
 
 //Création de nouvelle tâche lorsque l'utilisateur clique sur le bouton "Ajouter"
 boutonAjouter.addEventListener("click", function () {
@@ -21,53 +23,57 @@ boutonAjouter.addEventListener("click", function () {
     let logo = document.createElement("div");
     logo.append(fini, rennomer, poubelle);
 
-    nouvelleTache.append(titreTache, logo)
-    titreTache.innerText = nomNouvelleTache.value
+    nouvelleTache.append(titreTache, logo);
+    titreTache.innerText = nomNouvelleTache.value;
     tacheSection.appendChild(nouvelleTache);
     nouvelleTache.setAttribute("class", "nouvelleTache");
+
     //Déclarer une tâche comme étant terminée
-    let terminer = [];
     fini.addEventListener("click", function () {
-        // terminer.push(nouvelleTache);
-        nouvelleTache.classList.add("done");
         fini.style.color = "#60C5FF";
-        console.log(tacheSection);
+        nouvelleTache.style.backgroundColor = "#bdffcc"
+        fini.addEventListener("click", function () {
+            fini.style.color = "grey";
+            nouvelleTache.style.backgroundColor = "#E4E4E4"
+        });
     });
-    
+
     //Permettre de filter les tâches en affichant juste les éléments terminé
     boutonTermine.addEventListener("click", function () {
-        tacheSection.removeChild(nouvelleTache)
-        // forEach(element => {
-        //     if (element.classList != "done"){
-        //         nouvelleTache.style.display = "none"
-        //     }
-        // });
-    })
+        if (nouvelleTache.style.backgroundColor == "") {
+            nouvelleTache.style.display = "none";
+        } 
+    });
 
-    //Permet d'afficher toutes les tâches
-    boutonTout.addEventListener("click", function () {
-        tacheSection.appendChild(nouvelleTache);
-    })
-
+    //Permettre de filter les tâches en affichant juste les éléments à faire
+    boutonAFaire.addEventListener("click", function () {
+        if (nouvelleTache.style.backgroundColor == "rgb(189, 255, 204)") {
+            nouvelleTache.style.display = "none";
+        } 
+    });
+    
     //Permettre à l'utilisateur de pouvoir rennomer la tâche
     rennomer.addEventListener("click", function () {
         rennomer.classList.replace("fa-pen-to-square", "fa-floppy-disk");
         fini.style.visibility = "hidden";
         poubelle.style.visibility = "hidden";
-        titreTache.replaceWith(document.createElement("input"));
-        
+        titreTache.contentEditable = true;
+
         rennomer.addEventListener("click", function () {
             rennomer.classList.replace("fa-floppy-disk", "fa-pen-to-square");
             fini.style.visibility = "visible";
             poubelle.style.visibility = "visible";
-            // let newTitre = titreTache.value;
-            titreTache.replaceWith(document.createElement("h4"));
-            // titreTache.innerText = newTitre;
-            // console.log(newTitre);
-        })
+            titreTache.contentEditable = false;
+        });
     });
+
     //Permet de supprimer la tâche
     poubelle.addEventListener("click", function () {
-        nouvelleTache.remove(titreTache);
-    })
+        nouvelleTache.remove();
+    });
+
+    //Permet d'afficher toutes les tâches
+    boutonTout.addEventListener("click", function () {
+        nouvelleTache.style.display = "flex";
+    });
 });
